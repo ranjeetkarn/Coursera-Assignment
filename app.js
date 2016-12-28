1cb20f0059726bbdda0a6353c7d5cec0a2e2a306
Ranjeet Karn <ckarnr@jci.com>
2016-12-28 22:33:25 +0530

    Functionality implemented

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', function($scope) {
+    $scope.items = [];
+    $scope.visible = false;
+    $scope.visibleTooMuch = false;
+
+    $scope.convertToArray = function() {
+    $scope.array = $scope.string.split(',');
+}
+
+$scope.checkIfTooMuch = function () {
+  if (!$scope.array) {
+    $scope.emptyCheck = !$scope.visible;
+  }else if ($scope.array.length <= 3 ) {
+    $scope.emptyCheck = false;
+    $scope.visibleTooMuch = false;
+    $scope.visible = !$scope.visible;
+}else {
+  $scope.emptyCheck = false;
+  $scope.visible = false;
+  $scope.visibleTooMuch = !$scope.visible;
+}
+
+};
+
+
+});
+
+})();
