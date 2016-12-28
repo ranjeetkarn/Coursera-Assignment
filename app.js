f912049647da5450d10f8e1dd36b73e4902c892c
Ranjeet Karn <ckarnr@jci.com>
2016-12-28 23:43:49 +0530

    DI implemented for minification

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 (function () {
+'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', function($scope) {
+angular.module('LunchCheck' , [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
     $scope.items = [];
     $scope.visible = false;
     $scope.visibleTooMuch = false;
 
-    $scope.convertToArray = function() {
+$scope.convertToArray = function () {
     $scope.array = $scope.string.split(',');
-}
+};
 
 $scope.checkIfTooMuch = function () {
-  if (!$scope.array) {
+if (!$scope.array) {
     $scope.emptyCheck = !$scope.visible;
-  }else if ($scope.array.length <= 3 ) {
+}else if ($scope.array.length <= 3) {
     $scope.emptyCheck = false;
     $scope.visibleTooMuch = false;
     $scope.visible = !$scope.visible;
 }else {
-  $scope.emptyCheck = false;
-  $scope.visible = false;
-  $scope.visibleTooMuch = !$scope.visible;
+    $scope.emptyCheck = false;
+    $scope.visible = false;
+    $scope.visibleTooMuch = !$scope.visible;
 }
 
 };
-
-
-});
-
+}
 })();
